909328584e35c06fc7a77c4e4fae260d0c7e5ad9
Benjamin Peek <benjaminpeek8@gmail.com>
2023-10-23 22:13:29 -0600

    finished login javaScript

--- moms-kitchen/profile.js
+++ moms-kitchen/profile.js
@@ -1,7 +1,8 @@
+
 if (localStorage.getItem('loggedIn') === 'true') {
     document.getElementById("welcome-message").textContent = "Welcome back, " + localStorage.getItem('userName') + "!";
     document.getElementById("profile-styles").href = 'profile.css';
 }
 else {
-    document.getElementById("welcome-message").textContent = "Please Login on the Home page to use these features.";
+    document.getElementById("welcome-message").textContent = "Please login on the Home page to use these features.";
 }
