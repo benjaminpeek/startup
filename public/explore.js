d615e91a81d2ab80d0354e3a419244fad256d51d
Benjamin Peek <benjaminpeek8@gmail.com>
2023-11-11 16:31:11 -0700

    got recipe api working

--- public/explore.js
+++ public/explore.js
@@ -1,41 +1,45 @@
 console.log(apiKey);
 
-function displayPicture() {
+  function displayRecipes() {
     const random = Math.floor(Math.random() * 1000);
     fetch(`https://picsum.photos/v2/list?page=${random}&limit=1`)
+    // fetch(`https://api.spoonacular.com/recipes/random?number=4&apiKey=${apiKey}`)
       .then((response) => response.json())
       .then((data) => {
-        const containerEl = document.querySelector('#picture');
+        let recipe1 = data.recipes[0]
+        let recipe2 = data.recipes[1]
+        let recipe3 = data.recipes[2]
+        let recipe4 = data.recipes[3]
+
+        const recipeCard1El = document.getElementById("recipe-card-1");
+        let recipeCard1IngredientsHTML = "";
+        for (const ingredient of recipe1.extendedIngredients) {
+          recipeCard1IngredientsHTML += `<li>${ingredient.name}</li>`;
+        }
+        recipeCard1El.innerHTML = `<div class="recipe-name"><p>${recipe1.title}</p></div><img src="${recipe1.image}" class="recipe-image" /> <ul class="recipe-ingredients">${recipeCard1IngredientsHTML}</ul>`;
   
-        const width = containerEl.offsetWidth;
-        const height = containerEl.offsetHeight;
-  
-        const imgUrl = `https://picsum.photos/id/${data[0].id}/${width}/${height}?grayscale`;
-        const imgEl = document.createElement('img');
-        imgEl.setAttribute('src', imgUrl);
-        containerEl.appendChild(imgEl);
+        const recipeCard2El = document.getElementById("recipe-card-2");
+        let recipeCard2IngredientsHTML = "";
+        for (const ingredient of recipe2.extendedIngredients) {
+          recipeCard2IngredientsHTML += `<li>${ingredient.name}</li>`;
+        }
+        recipeCard2El.innerHTML = `<div class="recipe-name"><p>${recipe2.title}</p></div><img src="${recipe2.image}" class="recipe-image" /> <ul class="recipe-ingredients">${recipeCard2IngredientsHTML}</ul>`;
+        
+        const recipeCard3El = document.getElementById("recipe-card-3");
+        let recipeCard3IngredientsHTML = "";
+        for (const ingredient of recipe3.extendedIngredients) {
+          recipeCard3IngredientsHTML += `<li>${ingredient.name}</li>`;
+        }
+        recipeCard3El.innerHTML = `<div class="recipe-name"><p>${recipe3.title}</p></div><img src="${recipe3.image}" class="recipe-image" /> <ul class="recipe-ingredients">${recipeCard3IngredientsHTML}</ul>`;
+
+        const recipeCard4El = document.getElementById("recipe-card-4");
+        let recipeCard4IngredientsHTML = "";
+        for (const ingredient of recipe4.extendedIngredients) {
+          recipeCard4IngredientsHTML += `<li>${ingredient.name}</li>`;
+        }
+        recipeCard4El.innerHTML = `<div class="recipe-name"><p>${recipe4.title}</p></div><img src="${recipe4.image}" class="recipe-image" /> <ul class="recipe-ingredients">${recipeCard4IngredientsHTML}</ul>`;
       });
   }
   
-  function displayQuote(data) {
-    fetch('https://api.quotable.io/random')
-      .then((response) => response.json())
-      .then((data) => {
-        const containerEl = document.querySelector('#quote');
-  
-        const quoteEl = document.createElement('p');
-        quoteEl.classList.add('quote');
-        const authorEl = document.createElement('p');
-        authorEl.classList.add('author');
-  
-        quoteEl.textContent = data.content;
-        authorEl.textContent = data.author;
-  
-        containerEl.appendChild(quoteEl);
-        containerEl.appendChild(authorEl);
-      });
-  }
-  
-  // displayPicture();
-  // displayQuote();
+  displayRecipes();
   
--- public/profile.js
+++ public/profile.js
@@ -111,6 +111,6 @@ function addRecipe() {
     window.scrollTo(0, 0);
     recipeForm.reset();
     ingredientList = [];
-    document.getElementById("ingredient-list").innerHTML = "<li><input id='ingredient1' type='text' placeholder='ingredient 1' required /></li><li><input id='ingredient2' type='text' placeholder='ingredient 2' /></li><li><input id='ingredient3' type='text' placeholder='ingredient 3' /></li><button id='add-ingredient-btn' onclick='addIngredient()' type='button'>add ingredient</button>"
+    document.getElementById("ingredient-list").innerHTML = "<li><input id='ingredient1' type='text' placeholder='ingredient 1' required /></li><li><input id='ingredient2' type='text' placeholder='ingredient 2' /></li><li><input id='ingredient3' type='text' placeholder='ingredient 3' /></li><button id='add-ingredient-btn' onclick='addIngredient()' type='button'>add ingredient</button>";
     localStorage.removeItem("recipeImage");
 }
