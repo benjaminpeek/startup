6fe80b793113fc75174232f9e81624a7d151f4fc
Benjamin Peek <benjaminpeek8@gmail.com>
2023-11-17 15:40:26 -0700

    made adding recipes work correctly

--- public/profile.js
+++ public/profile.js
@@ -2,27 +2,40 @@
 
 let potentialIngredientCount = 3;
 let ingredientList = [];
-// let userRecipeCards = JSON.parse(localStorage.getItem("recipeCards"));
-if (localStorage.getItem("hasRecipes") === 'false') {
+let clearRecipesPressed = false;
+if (localStorage.getItem("hasRecipes") === 'false' || localStorage.getItem("numOfUserRecipes") === null) {
     localStorage.setItem("numOfUserRecipes", 0);
 }
 
-if (localStorage.getItem('loggedIn') === 'true') {
-    document.getElementById("welcome-message").textContent = "Welcome back, " + localStorage.getItem('userName') + "!";
-    document.getElementById("profile-styles").href = 'profile.css';
+displayRecipes();
 
-    // display the user's recipes
-    // if (localStorage.getItem("hasRecipes") === 'true') {
-    //     console.log("user has recipes");
-    //     for (let i = 1; i <= localStorage.getItem("numOfUserRecipes"); i++) {
-    //         // append the user recipe element items
-    //         // get the recipe card element from local storage string object
-    //         let currRecipeCard = JSON.parse(localStorage.getItem(`userRecipe${i}`));
-    //         // document.querySelector("#recipe-card-container").appendChild(currRecipeCard);
-    //     }
-    // }   
-} else {
-    document.getElementById("welcome-message").textContent = "Please login on the Home page to use these features.";
+function displayRecipes() {
+    if (localStorage.getItem("clearRecipesPressed") === 'true') {
+        document.querySelector("#recipe-card-container").innerHTML = "";
+    }
+
+    if (localStorage.getItem('loggedIn') === 'true') {
+        document.getElementById("welcome-message").textContent = "Welcome back, " + localStorage.getItem('userName') + "!";
+        document.getElementById("profile-styles").href = 'profile.css';
+    
+        // display the user's recipes
+        if (localStorage.getItem("hasRecipes") === 'true') {
+            console.log("user has recipes");
+            console.log(localStorage.getItem("numOfUserRecipes"));
+            for (let i = 1; i <= localStorage.getItem("numOfUserRecipes"); i++) {
+                // append the user recipe element items
+                // get the recipe card element from local storage string object
+                let currRecipeHTML = localStorage.getItem(`userRecipe${i}HTML`);
+                console.log(currRecipeHTML);
+
+                document.querySelector("#recipe-card-container").insertAdjacentHTML("beforeend", currRecipeHTML);
+
+                // document.querySelector("#recipe-card-container").appendChild(currRecipeCard);
+            }
+        }   
+    } else {
+        document.getElementById("welcome-message").textContent = "Please login on the Home page to use these features.";
+    }
 }
 
 
@@ -72,7 +85,7 @@ function addRecipe() {
     let recipeImageEl = document.createElement("img");
     recipeImageEl.setAttribute("class", "recipe-image");
     recipeImageEl.setAttribute("src", localStorage.getItem("recipeImage"));
-    let recipeImageElHTML = recipeImageEl.outerHTML;
+    let recipeImageHTML = recipeImageEl.outerHTML;
 
     // save all of the ingredients into a list
     for (let i = 1; i <= potentialIngredientCount; i++) {
@@ -94,23 +107,34 @@ function addRecipe() {
     let newRecipeCard = document.createElement("div");
     newRecipeCard.setAttribute("class", "recipe-card");
     // put it all together
-    newRecipeCard.innerHTML = recipeNameDivHTML + recipeImageElHTML + newRecipeIngredientsHTML;
+    newRecipeCard.innerHTML = recipeNameDivHTML + recipeImageHTML + newRecipeIngredientsHTML;
 
     // put it into the collection of the user's recipes
-    document.querySelector("#recipe-card-container").appendChild(newRecipeCard);
+    // document.querySelector("#recipe-card-container").appendChild(newRecipeCard);
 
     localStorage.setItem("hasRecipes", true);
     
     localStorage.setItem("numOfUserRecipes", parseInt(localStorage.getItem("numOfUserRecipes")) + 1);
-    console.log(typeof newRecipeCard);
-    localStorage.setItem(`userRecipe${localStorage.getItem("numOfUserRecipes")}`, JSON.stringify(newRecipeCard));
-    console.log(localStorage.getItem(`userRecipe${localStorage.getItem("numOfUserRecipes")}`));
+    // console.log(localStorage.getItem("numOfUserRecipes"));
+    localStorage.setItem(`userRecipe${localStorage.getItem("numOfUserRecipes")}HTML`, newRecipeCard.outerHTML);
+    // console.log(localStorage.getItem(`userRecipe${localStorage.getItem("numOfUserRecipes")}HTML`));
 
     // refresh page
-    // window.location.href = "profile.html";
     window.scrollTo(0, 0);
     recipeForm.reset();
     ingredientList = [];
     document.getElementById("ingredient-list").innerHTML = "<li><input id='ingredient1' type='text' placeholder='ingredient 1' required /></li><li><input id='ingredient2' type='text' placeholder='ingredient 2' /></li><li><input id='ingredient3' type='text' placeholder='ingredient 3' /></li><button id='add-ingredient-btn' onclick='addIngredient()' type='button'>add ingredient</button>";
     localStorage.removeItem("recipeImage");
+    displayRecipes();
+    window.location.href = "profile.html";
+}
+
+function clearRecipes() {
+    document.querySelector("#recipe-card-container").innerHTML = "";
+    for (let i = 1; i <= localStorage.getItem("numOfUserRecipes"); i++) {
+        localStorage.removeItem(`userRecipe${i}HTML`);
+    }
+    localStorage.setItem("numOfUserRecipes", 0);
+    localStorage.setItem("hasRecipes", false);
+    localStorage.setItem("clearRecipesPressed", true);
 }
