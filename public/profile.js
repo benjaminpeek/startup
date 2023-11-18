7062bddf365fd976791311df3513bed1d861cc0c
Benjamin Peek <benjaminpeek8@gmail.com>
2023-11-18 15:52:18 -0700

    followed mongo tutorial

--- public/profile.js
+++ public/profile.js
@@ -1,5 +1,21 @@
 "use strict";
 
+// given in data services
+const config = require('../dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('rental');
+
+(async function testConnection() {
+  await client.connect();
+  await db.command({ ping: 1 });
+})().catch((ex) => {
+  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  process.exit(1);
+});
+
+
 let potentialIngredientCount = 3;
 let ingredientList = [];
 let clearRecipesPressed = false;
